2f38bebad5dca24cd61636b408d63970cdcb3ca2
Tyler Hunt <tyhunt04@gmail.com>
2019-06-10 22:05:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(obj, name) {
-    obj.filter(function (name) {return obj.name === [name]})
+  return obj.filter(function (name) {return obj.name === [name]})
 
 }
