a255377f5c8168e9c7892f04185c5b095a69b6bf
Tyler Hunt <tyhunt04@gmail.com>
2019-06-10 22:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(obj, name) {
   return obj.filter(function (description) {
-    return description.toLowerCase() === [description.toLowerCase()]})
+    return description.toLowerCase() === description.toLowerCase()})
 }
